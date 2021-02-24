7d848eae2f3a56cd34335017f266e3201e15e670
Fabian Hernandez <h.fabian25f@gmail.com>
2021-02-24 00:28:54 -0600

    Edit template files, adding Bootstrap

--- src/main/java/com/tts/subscriberList1/controller/SubscriberController.java
+++ src/main/java/com/tts/subscriberList1/controller/SubscriberController.java
@@ -1,5 +1,8 @@
 package com.tts.subscriberList1.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -32,4 +35,14 @@ public class SubscriberController {
 		model.addAttribute("username",subscriberToAdd.getUsername());
 		return "subscriber/result";
 	}
+	
+	@GetMapping(value = "/subscribers")
+	public String getAllSubscribers(Subscriber subscriber, Model model) {
+		List<Subscriber> subscribers = new ArrayList<Subscriber>();
+		subscribers = (List<Subscriber>) subscriberRepository.findAll();
+		model.addAttribute("subscribers", subscribers);
+		return "subscriber/subscribers";
+	}
+	
+	
 }
